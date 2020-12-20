9283e27b6d85e3ebe4208a30ebbf2a5260f61ae7
Henrique Leao <henrique.s3.leao@gmail.com>
2020-12-20 19:31:41 -0300

    feat: add initial project config

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../styles/Home.module.css'
+
+const Home = () => {
+  return (
+    <div className={styles.container}>
+      <p>React Avançado</p>
+    </div>
+  )
+}
+
+export default Home
