4b0a9ea2171a52e80ee3c40a907e07d70fd686eb
Henrique Leao <henrique.s3.leao@gmail.com>
2020-12-22 21:35:37 -0300

    feat: add declaration file styled-components and change config babel

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,7 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: [
-    'src/**/*.ts(x)',
-    '!<rootDir>/src/components/**/*.stories.tsx'
-  ],
-  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  modulePaths: ['<rootDir>/src/']
 }
